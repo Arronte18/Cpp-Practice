af83143ef040e7185df42c4a1741d4d902731d62
Arronte18 <anthonyarronte@gmail.com>
2024-12-03 18:57:23 -0500

    CPP practice

---
+++ Coding Practice/userinput.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+    int favoriteNumber;
+    std::cout << "Enter your favorite number between 1 - 100: ";
+
+    std::cin >> favoriteNumber;
+
+    std::cout << "Amazing!! That's my favorite number too!" << std::endl;
+    std::cout << "No really!! " << favoriteNumber << " is my favorite number!" << std::endl;
+    return 0;
+}
